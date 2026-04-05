2416830b84cb37056ff194f7dfe6b3c34847046d
val-entyn-p <valentynpeliushok@gmail.com>
2026-04-05 15:39:27 -0400

    Gemini API setup complete

--- main/main.py
+++ main/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
+from pathlib import Path
+import google.generativeai as genai
 import os
 
-load_dotenv()
+load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 app = FastAPI()
 
@@ -11,12 +16,15 @@ class ReviewRequest(BaseModel):
     code: str
     
 @app.get("/")
-def roo():
+def root():
     return {"status": "AI Code Reviewer is running"}
 
 @app.post("/review")
 def review_code(request: ReviewRequest):
+    prompt = f"Review this code for bugs and issues:\n\n{request.code}"
+    
+    response = model.generate_content(prompt)
+    
     return {
-        "recieve_code": request.code,
-        "message": "hello_world"
+        "review": response.text
     }
