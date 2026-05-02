4b31c21ab5afe2f80a138a143b4c0c942aa51c96
val-entyn-p <valentynpeliushok@gmail.com>
2026-05-01 20:08:28 -0400

    connectin frontend

--- main/main.py
+++ main/main.py
@@ -47,7 +47,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://*.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
